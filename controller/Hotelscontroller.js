1cf6547e81597e602a28db5999f75533b0eef6b3
Hakim <tamihakim6@gmail.com>
2022-09-13 09:47:44 +0100

    upadated v1.1

--- controller/Hotelscontroller.js
+++ controller/Hotelscontroller.js
@@ -62,7 +62,80 @@ const updateHotel= async(req,res,next)=>{
 }
 
 
+const countByCity = async(req,res,next)=>{
+    
+
+     const cities = req.query.cities.split(",") 
+     try {
+        const list = await Promise.all(cities.map(city=>{
+            return hotel.countDocuments({city:city})
+        }))
+
+        res.json({list})
+
+     } catch (error) {
+        next(error)
+     }
 
 
 
-module.exports={addHotel,showHotels,deleteHotel,updateHotel}
+
+}
+
+
+const countTypes =async(req,res,next)=>{
+    try {
+         const countHotels =  await hotel.countDocuments({type:'hotel'})
+         const countAppartement = await hotel.countDocuments({type:'appartement'})
+         const countComplexe =  await hotel.countDocuments({type:'complexe'})
+         const countVilla =  await hotel.countDocuments({type:'villa'})
+         const countChalet =  await hotel.countDocuments({type:'chalet'})
+         const countCottage =  await hotel.countDocuments({type:'cottage'})
+         const countCamping =  await hotel.countDocuments({type:'camping de luxe'})
+         const countMaison =  await hotel.countDocuments({type:'maison'})
+         const countAppHotel =  await hotel.countDocuments({type:'appart hotel'})
+         const countAuberge =  await hotel.countDocuments({type:'auberge'})
+
+        res.json([
+
+           {type:'hotel',count:countHotels },
+           {type:"appartement",count:countAppartement},
+           {type:"complexe",count:countComplexe},
+           {type:"villa",count:countVilla},
+           {type:"chalet",count:countChalet},
+           {type:"cottage",count:countCottage},
+           {type:"camping",count:countCamping},
+           {type:"maison",count:countMaison},
+           {type:"appart hotel",count:countAppHotel},
+           {type:"auberge",count:countAuberge},
+           
+
+        ])
+
+
+    } catch (error) {
+        next(error)
+    }
+   
+}
+
+const searchHotel = async(req,res,next )=>{
+
+    const {destination} = req.query
+
+try {
+
+    const items = await hotel.find({city:destination})
+
+       res.json({items})
+
+
+} catch (error) {
+    next(error)
+}
+
+}
+
+
+
+module.exports={addHotel,showHotels,deleteHotel,updateHotel,countByCity,countTypes,searchHotel}
